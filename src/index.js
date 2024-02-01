57f8026dd6645613ef03f10b2ceac549755ea075
Rahulgit3D2Y <rc2504000@gmail.com>
2024-02-01 11:38:06 +0530

    added controller

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
-    path:"./env"
-})
-
+  path: "./env",
+});
 
 connectDB()
-.then(()=>{
-    application.listen(process.env.PORT||8000,()=>{
-        console.log(`server is running at port ${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log("failed to connect",err);
-})
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("failed to connect", err);
+  });
